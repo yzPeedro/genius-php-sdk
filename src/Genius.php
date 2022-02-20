426e978b8752156d33ad48f6a4e09d85e43acceb
yzPeedro <pedrocruzpessoa16@gmail.com>
2022-02-20 13:24:32 -0300

    Adding search artist method

--- src/Genius.php
+++ src/Genius.php
@@ -23,6 +23,10 @@ class Genius
             throw new GeniusException("You must provide your access token first.");
     }
 
+    /**
+     * @param string $query
+     * @return mixed
+     */
     public function search(string $query = "")
     {
         $search = curl_init(self::GENIUS_URL . "/search?q=" . $query);
@@ -34,5 +38,28 @@ class Genius
         ]);
 
         return json_decode(curl_exec($search));
+    }
+
+    /**
+     * @param string $id
+     * @param bool $songs
+     * @return mixed
+     */
+    public function artist(string $id, bool $songs = false)
+    {
+        $url = self::GENIUS_URL . "/artists/{$id}/songs";
+
+        if(! $songs)
+            $url = self::GENIUS_URL . "/artists/{$id}";
+
+        $artist = curl_init($url);
+        curl_setopt_array($artist, [
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => [ "Authorization: Bearer " . $_SESSION['genius_access_token'] ]
+        ]);
+
+        return json_decode(curl_exec($artist));
     }
 }
