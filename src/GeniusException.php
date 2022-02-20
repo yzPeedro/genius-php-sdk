f64ef653b214eb76db48f14a373cad2ba46e9f45
yzPeedro <pedrocruzpessoa16@gmail.com>
2022-02-20 13:13:07 -0300

    Adding custom exceptions

--- src/GeniusException.php
+++ src/GeniusException.php
@@ -2,7 +2,18 @@
 
 namespace Yzpeedro\GeniusPhpSdk;
 
-class GeniusException
-{
+use Throwable;
 
+class GeniusException extends \Exception
+{
+    public function __construct($message, $code = 0, Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return "GeniusException: [{$this->code}]: {$this->message}\n";
+    }
 }
